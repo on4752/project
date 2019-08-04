816bab49932a85816f8130a04916f8b9ff4ef6d1
Alex <example@gmail.com>
2019-08-05 00:56:14 +0700

    first task

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 "use strict";
 
-let money = prompt("Ваш бюджет на месяц?", '');
-let time = prompt("Введите дату в формате YYYY-MM-DD", '');
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
   money: money,
-  time: time
+  time: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
 };
 
-console.log(appData.money);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.money / 30);
